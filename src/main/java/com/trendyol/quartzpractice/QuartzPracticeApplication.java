0882d9555bc45aca357958d4d055134ba25434e3
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-16 11:35:53 +0300

    initial commit

---
+++ src/main/java/com/trendyol/quartzpractice/QuartzPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.trendyol.quartzpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzPracticeApplication.class, args);
+	}
+}
