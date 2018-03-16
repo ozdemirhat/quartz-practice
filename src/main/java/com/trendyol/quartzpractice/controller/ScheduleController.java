c4e399f0fd49d9b8ffd3e5f2cf2de432fcaa0164
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-16 16:32:00 +0300

    scheduler is added

---
+++ src/main/java/com/trendyol/quartzpractice/controller/ScheduleController.java
@@ -0,0 +1,31 @@
+package com.trendyol.quartzpractice.controller;
+
+
+import com.trendyol.quartzpractice.job.SampleJob;
+import com.trendyol.quartzpractice.service.SchedulerService;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScheduleController {
+
+    @Autowired
+    SchedulerService schedulerService;
+
+    @RequestMapping(value = "/schedule")
+    public String schedule (@RequestParam(name="identifier", required = true) String identifier) throws SchedulerException {
+        schedulerService.schedule(identifier ,SampleJob.class, "0/5 * * * * ?");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/reschedule")
+    public String reschedule (@RequestParam(name="identifier", required = true) String identifier) throws SchedulerException {
+        schedulerService.reschedule(identifier,"0/2 * * * * ?");
+        return "ok";
+    }
+
+
+}
